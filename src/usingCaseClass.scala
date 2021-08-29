c3ef7d7da6bf3e25dc0868cd0070b53357f13e11
Farzan <farzanroxz123@gmail.com>
2021-08-29 17:24:39 +0530

    Tested the Point case class using an extend app object. All works well

--- src/usingCaseClass.scala
+++ src/usingCaseClass.scala
@@ -1,3 +1,39 @@
 object usingCaseClass extends App{
 
+    val p1 = Point( 0 , 0)
+
+    val p2 = Point( 2 , 6 )
+
+    val p3 = Point( 9 , 11)
+
+    val p4 = p1.copy()
+
+    val p5 = p1.copy( b= 5 )
+
+    val p6 = Point( 1 , 18 )
+
+    val p7 = Point( 9 , 27 )
+
+
+    println(p1)
+
+    println(p2)
+
+    println(p3)
+
+    println(p1 == p4)
+
+    println(p1 == p5)
+
+    println( p2 + p3 )
+
+    println( p1.move( 18 , 27 ) )
+
+    println( p6 distanceBetween p7 )
+
+    println( p2.invert )
+
+
+
+
 }
