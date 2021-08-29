45637daa31718ac4179233e904d4f03edff478b0
Farzan <farzanroxz123@gmail.com>
2021-08-29 17:11:10 +0530

    Completed the Point case class with all required methods

--- src/Point.scala
+++ src/Point.scala
@@ -1,5 +1,14 @@
-case class Point(){
+case class Point( a:Int , b:Int ){
 
+    def x:Int = a
+    def y:Int = b
 
+    def +( that:Point) : Point = Point( this.x + that.x , this.y + that.y )
+
+    def move( dx : Int , dy : Int ) :Point = Point( this.x + dx , this.y + dy )
+
+    def distanceBetween( that : Point ) : Double = math.sqrt( math.pow( ( this.x - that.x ) , 2) + math.pow( ( this.y - that.y) , 2))
+
+    def invert : Point = Point( this.y , this.x )
 
 }
